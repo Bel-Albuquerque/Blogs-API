9f9dacde2defb083f28d30d2e5a796324945e1b1
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-05 14:38:52 -0300

    corrige erros de lint

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,10 +1,10 @@
 // const userService = require('../service/userService');
 
-const create = async (req, res) => {
+// const create = async (req, res) => {
   // const { status, json } = await userService.create(req.body);
   // return res.status(status).json(json);
-};
+// };
 
-module.exports = {
-  create,
-};
+// module.exports = {
+//   create,
+// };
