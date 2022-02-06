16490f866e7383c08c1cf11b98a65ca183e8b23a
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-06 20:52:13 -0300

    adiciona um drop no campo down

--- migrations/20220204180706-Users.js
+++ migrations/20220204180706-Users.js
@@ -35,11 +35,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Users')
   }
 };
