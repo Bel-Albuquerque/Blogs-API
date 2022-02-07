ce0007144196d3eac541b58c6e8606615b6868cc
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-06 22:14:26 -0300

    cria o controllerCategorie e implementa create

---
+++ controller/categorieController.js
@@ -0,0 +1,14 @@
+const categorieService = require('../service/categorieService');
+const { tokenNotFound } = require('../validations/errorMessages');
+
+const create = async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json(tokenNotFound);
+
+  const { status, json } = await categorieService.create(authorization, req.body);
+  return res.status(status).json(json);
+};
+
+module.exports = {
+  create,
+};
