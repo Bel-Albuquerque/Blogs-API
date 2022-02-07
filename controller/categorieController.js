e548885006fda3f248417843a2646fb629481dfe
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-06 22:36:36 -0300

    implementa getAll na camada controller de categories

--- controller/categorieController.js
+++ controller/categorieController.js
@@ -9,6 +9,14 @@ const create = async (req, res) => {
   return res.status(status).json(json);
 };
 
+const getAll = async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json(tokenNotFound);
+  const { status, json } = await categorieService.getAll(authorization, req.body);
+  return res.status(status).json(json);
+};
+
 module.exports = {
   create,
+  getAll,
 };
