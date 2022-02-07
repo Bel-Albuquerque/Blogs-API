b94187e03d5b1db225f067a718d023ca1ca149c8
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-06 22:16:16 -0300

    padroniza mensagem de erro

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,7 @@
 const userService = require('../service/userService');
+const { tokenNotFound } = require('../validations/errorMessages');
+
+
 
 const create = async (req, res) => {
   const { status, json } = await userService.create(req.body);
@@ -7,7 +10,7 @@ const create = async (req, res) => {
 
 const getAll = async (req, res) => {
   const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+  if (!authorization) return res.status(401).json(tokenNotFound);
   const response = await userService.getOneOrAllUsers(authorization, userService.findAll);
   return res.status(response.status).json(response.json);
 };
@@ -15,7 +18,7 @@ const getAll = async (req, res) => {
 const getById = async (req, res) => {
   const { authorization } = req.headers;
   const { id } = req.params;
-  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+  if (!authorization) return res.status(401).json(tokenNotFound);
   const response = await userService.getOneOrAllUsers(authorization, userService.findUser, id);
   return res.status(response.status).json(response.json);
 };
