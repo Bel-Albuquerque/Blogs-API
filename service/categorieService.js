94a4dc58909e382cb698ad09492e3051281005bd
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-06 22:43:32 -0300

    implementa funçao getAll na camada service

--- service/categorieService.js
+++ service/categorieService.js
@@ -1,5 +1,10 @@
 const { Categorie, User } = require('../models');
-const { successRequest, createCategorieErrorCases } = require('../validations/errorValidations');
+const { expiredToken } = require('../validations/errorMessages');
+const {
+  successRequest,
+  createCategorieErrorCases,
+  erroRequest,
+} = require('../validations/errorValidations');
 const { decoder } = require('../validations/tokenValidations');
 
 const create = async (token, body) => {
@@ -15,6 +20,20 @@ const create = async (token, body) => {
   }
 };
 
+const getAll = async (token) => {
+  try {
+    const userName = await decoder(token);
+    await User.findOne({ where: { displayName: userName } });
+
+    const categorie = await Categorie.findAll();
+    return successRequest(200, categorie);
+  } catch (err) {
+    console.log(err);
+    return erroRequest(401, expiredToken);
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
