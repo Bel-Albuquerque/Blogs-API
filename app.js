50765bcc484f3afc47f319ec05ebcef8ea5fbca6
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-02-07 22:01:46 -0300

    implementa rota POST /post

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = express();
 const userRouter = require('./routers/userRouter');
 const loginRouter = require('./routers/loginRouter');
 const categoriesRouter = require('./routers/categoriesRouter');
+const postRouters = require('./routers/postRouters');
 
 app.use(bodyParser.json());
 
 app.use('/', userRouter);
 app.use('/', loginRouter);
 app.use('/', categoriesRouter);
+app.use('/', postRouters);
 
 module.exports = app; 
---
+++ controller/postController.js
@@ -0,0 +1,10 @@
+// const userService = require('../service/userService');
+
+const createPost = async (req, res) => {
+  // const { status, json } = await userService.login(req.body);
+  // return res.status(status).json(json);
+};
+
+module.exports = {
+  createPost,
+};
